75091db242ae82079062193b8c578e582f9d3d40
Navjot Singh <navjotsingh.official@gmail.com>
2020-05-18 17:03:06 -0400

    Add sample project to start data binding

---
+++ app/src/main/java/com/android/ironclad/databindingsample/util/StringUtil.java
@@ -0,0 +1,13 @@
+package com.android.ironclad.databindingsample.util;
+
+public class StringUtil {
+    public static String getQuantityString(int quantity) {
+        return ("Qty: " + quantity);
+    }
+
+    public static String convertIntToString(int value) {
+        return (String.valueOf(value));
+    }
+
+
+}
